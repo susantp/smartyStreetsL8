f352278a8a0ff591d7306978cc5a742ae762c486
Susant Paudel <techbizznepal@gmail.com>
2021-09-22 01:21:43 +0545

    some update

--- config/smartystreets.php
+++ config/smartystreets.php
@@ -1,45 +1,46 @@
+<?php
 return array(
 
-/*
-|--------------------------------------------------------------------------
-| API Keys
-|--------------------------------------------------------------------------
-|
-*/
-'authId' 	=> 'raw ID here',
-'authToken'	=> 'raw token here',
+    /*
+    |--------------------------------------------------------------------------
+    | API Keys
+    |--------------------------------------------------------------------------
+    |
+    */
+    'authId' 	=> 'raw ID here',
+    'authToken'	=> 'raw token here',
 
 
-/*
-|--------------------------------------------------------------------------
-| Endpoint
-|--------------------------------------------------------------------------
-|
-*/
-'endpoint' => 'https://api.smartystreets.com',
+    /*
+    |--------------------------------------------------------------------------
+    | Endpoint
+    |--------------------------------------------------------------------------
+    |
+    */
+    'endpoint' => 'https://api.smartystreets.com',
 
-/*
-|--------------------------------------------------------------------------
-| cURL Failure Callback
-|
-| If you don't get back an HTTP 200, you can use this to handle the failure
-| https://smartystreets.com/docs/address#http-response-status
-|--------------------------------------------------------------------------
-|
-*/
-'failureCallback' => null,
+    /*
+    |--------------------------------------------------------------------------
+    | cURL Failure Callback
+    |
+    | If you don't get back an HTTP 200, you can use this to handle the failure
+    | https://smartystreets.com/docs/address#http-response-status
+    |--------------------------------------------------------------------------
+    |
+    */
+    'failureCallback' => null,
 
-/*
-|--------------------------------------------------------------------------
-| Optional HTTP request headers
-| https://smartystreets.com/docs/address#http-request-headers
-|--------------------------------------------------------------------------
-|
-*/
-'optionalRequestHeaders' => [
+    /*
+    |--------------------------------------------------------------------------
+    | Optional HTTP request headers
+    | https://smartystreets.com/docs/address#http-request-headers
+    |--------------------------------------------------------------------------
+    |
+    */
+    'optionalRequestHeaders' => [
 //Regardless if the address actually exists, format it as if it did. For example: "523 Walnut St" when only 524 and 520 Walnut exist.
-'X-Standardize-Only' => false,
+        'X-Standardize-Only' => false,
 //Very aggressive match finding; may include results that aren't really valid.
-'X-Include-Invalid' => false,
-],
+        'X-Include-Invalid' => false,
+    ],
 );
